31a19ef5b6ba2b24aa6819a7f1b9aa29c2bad052
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-27 23:56:50 +0530

    Create NavbarCmp.vue

---
+++ src/components/NavbarCmp.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <!-- Navbar is styled here -->
+    <div class="w-10/12 flex flex-row items-center justify-between py-8">
+        <div class="flex flex-row gap-10 items-center justify-center">
+            <img src="../assets/images/logo.svg" alt="">
+            <div class="flex flex-row gap-8 items-center">
+                <div>Features</div>
+                <div>Pricing</div>
+                <div>Resources</div>
+            </div>
+        </div>
+        <div class="flex flex-row gap-8 items-center justify-center">
+            <div>Login</div>
+            <div class="font-semibold py-2 bg-[#29D1D1] text-white rounded-full px-6">Sign Up</div>
+        </div>
+    </div>
+
+</template>
