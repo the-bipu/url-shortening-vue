84810e2ebd97c0837ba023d2dd787bf022f3684d
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-29 23:58:47 +0530

    Update NavbarCmp.vue

--- src/components/NavbarCmp.vue
+++ src/components/NavbarCmp.vue
@@ -1,7 +1,7 @@
 <template>
 
     <!-- Navbar is styled here -->
-    <div class="w-10/12 flex flex-row items-center justify-between py-8">
+    <div class="w-10/12 md:flex hidden flex-row items-center justify-between py-8">
         <div class="flex flex-row gap-10 items-center justify-center">
             <img src="../assets/images/logo.svg" alt="">
             <div class="flex flex-row gap-8 items-center">
@@ -14,6 +14,10 @@
             <div>Login</div>
             <div class="font-semibold py-2 bg-[#29D1D1] text-white rounded-full px-6">Sign Up</div>
         </div>
+    </div>
+    <div class="w-10/12 md:hidden flex flex-row items-center justify-between py-8">
+        <img src="../assets/images/logo.svg" alt="">
+        <div>H</div>
     </div>
 
 </template>
